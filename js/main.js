87c07a7de3d7d8e0a797295915c4743bbce8eef3
davidalexz <alexz.chimi@gmail.com>
2022-08-14 12:23:51 +0300

    added function for query parameters

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
-// eslint-disable-next-line no-alert
-alert('working');
+/* eslint-disable no-console */
+// eslint-disable-next-line no-use-before-define
+document.querySelector('button').addEventListener('click', apiRequest);
+
+async function apiRequest() {
+  const rapperName = document.querySelector('input').value;
+  try {
+    const response = await fetch(`https://test-rap-api-v1.herokuapp.com/api/${rapperName}`);
+    const data = await response.json;
+
+    console.log(data);
+    document.querySelector('h2').innerText = data.birthName;
+  } catch (error) {
+    console.error(error);
+  }
+}
